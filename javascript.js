84f05f1f35919560285a2ce8bae0685dfb01566f
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2023-10-29 10:45:25 -0300

    add responsividade

--- javascript.js
+++ javascript.js
@@ -1,43 +1,43 @@
 const pikachu = document.querySelector('.pikachu');
 const pokebola = document.querySelector('.pokebola');
+const texto = document.querySelector('.texto');
 
 const jump = () => {
     pikachu.classList.add('jump');
-    
+
     setTimeout(() => {
         pikachu.classList.remove('jump');
     }, 500);
 }
 
-const loop = setInterval(() =>{
+const loop = setInterval(() => {
 
-const pokebolaPosition = pokebola.offsetLeft;
-const pikachuPosition = +window.getComputedStyle(pikachu).bottom.replace('px','');
+    const pokebolaPosition = pokebola.offsetLeft;
+    const pikachuPosition = +window.getComputedStyle(pikachu).bottom.replace('px', '');
 
-console.log(pikachuPosition);
+    console.log(pikachuPosition);
 
-if(pokebolaPosition<= 250 && pokebolaPosition>130  && pikachuPosition < 80){
+    if (pokebolaPosition <= 250 && pokebolaPosition > 130 && pikachuPosition < 80) {
 
-    pokebola.style.animation = 'none';
-    pokebola.style.left = `${pokebolaPosition}px`;
+        pokebola.style.animation = 'none';
+        pokebola.style.left = `${pokebolaPosition}px`;
 
-    pikachu.style.animation = 'none';
-    pikachu.style.left = `${pikachuPosition}px`;
+        pikachu.style.animation = 'none';
+        pikachu.style.left = `${pikachuPosition}px`;
 
-    pikachu.src = 'image/game-over.png'
-    pikachu.style.width = '260px';
-     
-    if(pikachuPosition==0){
-        pikachu.style.bottom= '-50px';
+        pikachu.src = 'image/game-over.png'
+        pikachu.style.width = '260px';
+
+        pikachu.style.bottom = '-50px';
+
+        texto.innerHTML="Game over";  
+
+        clearInterval(loop);
+
+        document.addEventListener('keydown', () => {
+            location.reload();
+        })
     }
-
-
-    clearInterval(loop);
-
-    document.addEventListener('keydown', () =>{
-        location.reload();
-    })
-}
 
 }, 10)
 
