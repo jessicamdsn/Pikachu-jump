2cf54c047bad11b96278de8933bb8171c63fd57e
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2023-10-28 21:23:52 -0300

    First commit

---
+++ javascript.js
@@ -0,0 +1,44 @@
+const pikachu = document.querySelector('.pikachu');
+const pokebola = document.querySelector('.pokebola');
+
+const jump = () => {
+    pikachu.classList.add('jump');
+    
+    setTimeout(() => {
+        pikachu.classList.remove('jump');
+    }, 500);
+}
+
+const loop = setInterval(() =>{
+
+const pokebolaPosition = pokebola.offsetLeft;
+const pikachuPosition = +window.getComputedStyle(pikachu).bottom.replace('px','');
+
+console.log(pikachuPosition);
+
+if(pokebolaPosition<= 250 && pokebolaPosition>130  && pikachuPosition < 80){
+
+    pokebola.style.animation = 'none';
+    pokebola.style.left = `${pokebolaPosition}px`;
+
+    pikachu.style.animation = 'none';
+    pikachu.style.left = `${pikachuPosition}px`;
+
+    pikachu.src = 'image/game-over.png'
+    pikachu.style.width = '260px';
+     
+    if(pikachuPosition==0){
+        pikachu.style.bottom= '-50px';
+    }
+
+
+    clearInterval(loop);
+
+    document.addEventListener('keydown', () =>{
+        location.reload();
+    })
+}
+
+}, 10)
+
+document.addEventListener('keydown', jump);
